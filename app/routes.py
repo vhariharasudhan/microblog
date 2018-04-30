0f2d50f494f986f004d6f5436fcfb40e40333a11
Hari Viswanathan <vhariharasudhan@gmail.com>
2018-04-29 19:17:48 -0700

    Basics of flask templates with jinga2 engine

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Hari'}
+    posts = [
+        {
+            'author': 'Sneha',
+            'body': 'Python is cool!',
+        },
+        {
+            'author': 'Sundar',
+            'body': 'js is cooler',
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
