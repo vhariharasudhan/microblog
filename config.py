f265c883057c34b78ad1f26a9379d46aa93e17ea
Hari Viswanathan <vhariharasudhan@gmail.com>
2018-05-06 13:28:54 -0700

    Added profile edit page with upload file feature

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or '/Users/vhari/practice/microblog/profile_pics'
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+    MAX_CONTENT_LENGTH = 32 * 1024
+
